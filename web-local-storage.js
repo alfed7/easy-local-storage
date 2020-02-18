fdb75f00fb6295867d86ef411895c6cebdd43db3
Alexander Fedorenko <alfedmix@gmail.com>
2020-02-18 13:55:43 +0200

    Add possiblity to choose between session and local storage

--- web-local-storage.js
+++ web-local-storage.js
@@ -1,21 +1,22 @@
-function WebLocalStorage() {
+function WebLocalStorage(useSessionStorage) {
+    this.storage = useSessionStorage ? sessionStorage : localStorage;
     this.get = function (fieldName) {
         if(this._haveLocalStorage()) {
-            return localStorage.getItem(fieldName);
+            return this.storage.getItem(fieldName);
         }
     }
     this.set = function (fieldName, value, options = null) {
         if(this._haveLocalStorage()) {
-            localStorage.setItem(fieldName, value);
+            this.storage.setItem(fieldName, value);
         }
     }
     this.remove = function (fieldName) {
         if(this._haveLocalStorage()) {
-            localStorage.removeItem(fieldName);
+            this.storage.removeItem(fieldName);
         }
     }
     this._haveLocalStorage = function () {
-        return typeof localStorage !== "undefined";
+        return typeof this.storage !== "undefined";
     }
 }
 
