dfd01325b97d3f1f3f27c2622ecdfde01829d305
Alexander Fedorenko <alfedmix@gmail.com>
2021-02-12 17:11:34 +0200

    Correct errors after migration to a newer node-js

--- web-local-storage.js
+++ web-local-storage.js
@@ -1,23 +1,30 @@
+function _isBrowser() {
+  return typeof this.storage !== "undefined";
+}
 function WebLocalStorage(useSessionStorage) {
-    this.storage = useSessionStorage ? sessionStorage : localStorage;
-    this.get = function (fieldName) {
-        if(this._haveLocalStorage()) {
-            return this.storage.getItem(fieldName);
-        }
+  this.storage = _isBrowser()
+    ? useSessionStorage
+      ? sessionStorage
+      : localStorage
+    : null;
+  this.get = function (fieldName) {
+    if (this._haveLocalStorage()) {
+      return this.storage.getItem(fieldName);
     }
-    this.set = function (fieldName, value, options = null) {
-        if(this._haveLocalStorage()) {
-            this.storage.setItem(fieldName, value);
-        }
+  };
+  this.set = function (fieldName, value, options = null) {
+    if (this._haveLocalStorage()) {
+      this.storage.setItem(fieldName, value);
     }
-    this.remove = function (fieldName) {
-        if(this._haveLocalStorage()) {
-            this.storage.removeItem(fieldName);
-        }
+  };
+  this.remove = function (fieldName) {
+    if (this._haveLocalStorage()) {
+      this.storage.removeItem(fieldName);
     }
-    this._haveLocalStorage = function () {
-        return typeof this.storage !== "undefined";
-    }
+  };
+  this._haveLocalStorage = function () {
+    return typeof this.storage !== "undefined" && this.storage;
+  };
 }
 
 module.exports = WebLocalStorage;
